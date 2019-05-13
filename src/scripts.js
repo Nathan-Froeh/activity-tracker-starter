cece98b75097d4d39d4ad656f2e69ca17c56b711
Nathan <nathan.froeh@gmail.com>
2019-05-13 15:54:37 -0600

    Merge sleep chart and active chart

--- src/scripts.js
+++ src/scripts.js
@@ -40,23 +40,82 @@ function getActivityData(id) {
   activity = new Activity(data.activityData, user)
 }
 
-$(".hide__user__card").click(function() {
-  $('.user--card').slideToggle('slow')
-})
+  function getHydrationData(id) {
+    const data = hydrationData.find(x => x.userID === id + 1)
+    hydration = new Hydration(data.hydrationData)
+  }
+
+  function getActivityData(id) {
+    const data = activityData.find(x => x.userID === id + 1)
+    activity = new Activity(data.activityData, user)
+  }
+
+  $(".hide__user__card").click(function() {
+    $('.user--card').slideToggle('slow')
+  })
   
-function loadHydration(hydration) {
-  let today = hydration.hydrationData[hydration.hydrationData.length - 1].date
-  let allTimeHydration = hydration.calcAllTimeHydration() 
-  let dateHydration = hydration.calcDateHydration(today)
-  let weeklyHydration = hydration.calcWeeklyHydration(today)
-  $(".water--box").append(`<p>You drank ${dateHydration} ounces</p>`);
-  $(".water--box").append(`<p>On average you drink ${allTimeHydration} ounces</p>`);
-  $(".water--box").append(`<p>This week you drank ${weeklyHydration} ounces</p>`);
-  console.log(today)
-  // console.log(allTimeHydration)
-  // console.log(dateHydration)
-}
-loadHydration(hydration)
+  function loadHydration(hydration) {
+    let today = hydration.hydrationData[hydration.hydrationData.length - 1].date
+    let allTimeHydration = hydration.calcAllTimeHydration() 
+    let dateHydration = hydration.calcDateHydration(today)
+    let weeklyHydration = hydration.calcWeeklyHydration(today)
+    $(".water--box").append(`<p>You drank ${dateHydration} ounces</p>`);
+    $(".water--box").append(`<p>On average you drink ${allTimeHydration} ounces</p>`);
+    $(".water--box").append(`<p>This week you drank ${weeklyHydration} ounces</p>`);
+  }
+  loadHydration(hydration)
+
+  function loadDailyActivity(activity) {
+    let today = activity.activityData[activity.activityData.length - 1].date
+    let activeTime = activity.getActiveTimeByDate(today)
+    let milesWalked = activity.getMilesWalked(today);
+    loadDailyStepGoal(today)
+    $(".active__day__time__info").append(`<img src = '../images/stopwatch.svg' class = 'stopwatch'>
+      <p>Active Time: ${activeTime} minutes</p>`);
+    $(".active__day__miles__info").append(`<img src = '../images/footsteps.svg' class = 'footsteps'>
+    <p>Miles Walked: ${milesWalked}</p>`);
+  }
+  
+  function loadDailyStepGoal(today) {
+    let steps = activity.getSteps(today)
+    let stepGoal = activity.getStepGoal(today) === true ? 
+      'You have reached your step goal!': 'Keep on walking! You have not reached your goal!'
+    $(".active__day__goal__info").append(`<p>${stepGoal}</p>`);
+    $(".active__day__goal__info").append(`<p>You walked <b>${steps} </b>steps today.</p>`);
+    let dailyPercent = activity.getStepGoalPercentage(today)
+    $("#progress__bar__active__goal").css( "width", dailyPercent ) 
+    $("#active__goal__percentage").text(dailyPercent)
+  }
+  function loadWeeklyActivity(activity) {
+    let today = activity.activityData[activity.activityData.length - 1].date
+    let weeklyAverage = activity.getWeeklyAvgActive(today)
+    let weeklyData = createWeeklyData(today)
+    $(".active__week__tab").hide()
+    $(".active__week__tab").append(`<p>Weekly Average Active Time:${weeklyAverage}</p>`);
+    $(".active__week__tab").append(`<table class = "weekly--active"><tr><th><b>Day</b><th>Step</th>
+    <th>Minutes</th><th>Stairs</th>${weeklyData}</table>`);
+  }
+  function createWeeklyData(day) {
+    let weeksActivityData = activity.getWeeklyActive(day)
+    let sortedData = weeksActivityData.map((day, index) => {
+      return `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${day.numSteps} </td>
+          <td>${day.minutesActive}</td> 
+          <td>${day.flightsOfStairs}</td>
+        </tr>`
+    })
+    return sortedData.join(' ')
+  }
+
+  loadDailyActivity(activity)
+  loadWeeklyActivity(activity)
+  
+  $( ".current--active--box" ).click(function() {
+    $(".active__day__tab").toggle()
+    $(".active__week__tab").toggle()
+  });
 
 function loadSleep(sleep) {
   let today = sleep.sleepData[sleep.sleepData.length - 1].date
@@ -76,18 +135,6 @@ function loadSleep(sleep) {
   // console.log(sleepTime)
 }
 loadSleep(sleep)
-  
-function loadActivity(activity) {
-  let today = activity.activityData[activity.activityData.length - 1].date
-  let stepGoal = activity.getStepGoal === true ? "You reached your step goal!" : "You have not reached your step goal, keep on walking!"
-  let milesWalked = activity.getMilesWalked(today);
-  let activeTime = activity.getActiveTimeByDate(today)
-  let weeklyAverage = activity.getWeeklyAvgActive(today)
-  $(".current--active--box").append(`<p>Active Time: ${activeTime} minutes</p>`);
-  $(".current--active--box").append(`<p>Weekly Average Active Time:${weeklyAverage}</p>`);
-  $(".current--active--box").append(`<p>Miles Walked: ${milesWalked}</p>`);
-  $(".current--active--box").append(`<p>${stepGoal}</p>`);
-}
-loadActivity(activity)
+
   
 
